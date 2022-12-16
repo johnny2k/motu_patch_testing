f1a1ae22ea3aaf6db58a5f575175d0bc390ec262
johnny2k <johnny2k@gmail.com>
2022-12-15 23:50:03 -0800

    Update enable_audio_debug.sh
    Add punctuation to a comment.

--- bin/enable_audio_debug.sh
+++ bin/enable_audio_debug.sh
@@ -3,13 +3,13 @@
 sudo sh -c 'echo "module snd_usb_audio +p" > /sys/kernel/debug/dynamic_debug/control'
 sudo sh -c 'echo "module xhci_hcd +p" > /sys/kernel/debug/dynamic_debug/control'
 
-# Line numbers might be different on your system check with this command ...
+# Line numbers might be different on your system. Check with this command ...
 # sudo cat /sys/kernel/debug/dynamic_debug/control | grep "asked for" | grep -Po "xhci-ring.c:\K[[:digit:]]*"
 sudo sh -c 'echo "file xhci-ring.c line 2538 -p" > /sys/kernel/debug/dynamic_debug/control'
 sudo sh -c 'echo "file xhci-ring.c line 2531 -p" > /sys/kernel/debug/dynamic_debug/control'
 
 # Clear logs before a test if you want.
-#sudo dmesg -C
+# sudo dmesg -C
 
 # Watch the logs
-#sudo dmesg -w
+# sudo dmesg -w
